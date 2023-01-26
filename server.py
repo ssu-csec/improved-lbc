e0d44bb4bca22002a090a5e6593105524706ac50
Byeori <99kbr11@gmail.com>
2023-01-26 17:24:31 +0900

    add clients synchronization function

--- server.py
+++ server.py
@@ -6,31 +6,39 @@ import threading
 import core
 import packet
 
+def Conflict_Handling(send_queue, client_group):
+	pass
+
+
+
+
 def Send(client_group, send_queue):
 	while True:
 		recv = send_queue.get()
-		print("receive data is ", recv)
 		if recv == 'new client':
 			break
-		if recv == 'Gathering':
+		elif recv == 'Gathering':
 			client_group[random.randint(0, len(client_group) - 1)].send(pickle.dumps('Gathering'))
 		else:
 			print("check point1")
 			for client in client_group:
 				if recv[0] != client:
 					client.send(recv[1])
+				else:
+					pass
+	print("break for new client")
 
-def Recv(conn, data, send_queue, cnt):
+def Recv(conn, data, send_queue, count):
 	while True:
 		recv_data = conn.recv(buf)
 		send_queue.put([conn, recv_data])
-		cnt += 1
+		count += 1
 		mod_info = pickle.loads(recv_data)
 		packet.unpacking(data, mod_info)
-		if cnt >= 20:
+		if count > 20:
 			send_queue.put('Gathering')
-			cnt -= 20
-		print("get ", cnt, "th data")
+			count -= 20
+		#print("get ", cnt, "th data")
 
 port = 8080
 buf = 8192
@@ -53,10 +61,13 @@ while True:
 	print("Connected to new client ", str(addr))
 	send_data = pickle.dumps(data)
 	connectionSock.sendall(send_data)
-
-	send_queue.put('new client')
-	send_thread = threading.Thread(target = Send, args = (client_group, send_queue, ))
-	send_thread.start()
+	if len(client_group) > 1:
+		send_queue.put('new client')
+		send_thread = threading.Thread(target = Send, args = (client_group, send_queue, ))
+		send_thread.start()
+	else:
+		send_thread = threading.Thread(target = Send, args = (client_group, send_queue, )    )
+		send_thread.start()
 
 	recv_thread = threading.Thread(target = Recv, args = (connectionSock, data, send_queue, cnt, ))
 	recv_thread.start()
