d6d9f0fa89a1c31c14f70e58a34f325a36152342
Byeori <99kbr11@gmail.com>
2023-03-02 20:03:59 +0900

    Feat: Add inserting newline in sample file

--- exfile_gen.py
+++ exfile_gen.py
@@ -10,8 +10,11 @@ str_len = int(input("Choose file length: "))
 f_str = ""
 
 for i in range(str_len):
-	asc = random.randint(32, 127)
-	f_str += chr(asc)
+	if i%50 == 0:
+		f_str += "\n"
+	else:
+		asc = random.randint(32, 127)
+		f_str += chr(asc)
 
 data = crypto.Enc(mode, f_str, key, iv)
 with open("ex_server_" + str(str_len) + "_" + mode + ".p", 'wb') as f:
--- nonefile_gen.py
+++ nonefile_gen.py
@@ -6,8 +6,11 @@ str_len = int(input("Choose file length: "))
 f_str = ""
 
 for i in range(str_len):
-	asc = random.randint(32, 127)
-	f_str += chr(asc)
+	if i%50 == 0:
+		f_str += "\n"
+	else:
+		asc = random.randint(32, 127)
+		f_str += chr(asc)
 
 with open("none_server_" + str(str_len) + ".txt", 'w') as f:
 	f.write(f_str)
